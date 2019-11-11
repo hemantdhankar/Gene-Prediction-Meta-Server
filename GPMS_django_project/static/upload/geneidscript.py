56e4d4893d25b7dc3030bdc1163a2b4a35d53449
nitesh18400 <nitesh18400@iiitd.ac.in>
2019-11-11 19:34:09 +0530

    geneid running output folder

--- GPMS_django_project/static/upload/geneidscript.py
+++ GPMS_django_project/static/upload/geneidscript.py
@@ -1,4 +1,6 @@
 import os
+import shutil
 os.chdir("geneid/bin")
-os.system("./geneid -P ../human.param.Feb_22_2006  ../../test.fa > 1.txt")
-os.system("cat 1.txt")
+os.system("./geneid -P ../human.param.Feb_22_2006  ../../test.fa > geneidoutput.txt")
+os.system("cat geneidoutput.txt")
+shutil.copy('geneidoutput.txt', '../../../output')
