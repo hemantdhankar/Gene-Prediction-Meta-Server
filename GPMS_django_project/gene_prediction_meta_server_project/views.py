d36756b5d06bbaacf201fc5cd67d345eadc0a189
Hemant Dhankar <hemantdhankar123@gmail.com>
2019-11-20 00:24:45 +0530

    Updated Form

--- GPMS_django_project/gene_prediction_meta_server_project/views.py
+++ GPMS_django_project/gene_prediction_meta_server_project/views.py
@@ -8,9 +8,18 @@ from pathlib import Path
 def home(request):
 	if request.method == 'POST':
 		handle_uploaded_file(request.FILES['file'])
-		os.path.abspath('..')
-		os.chdir("static/upload/")
-		os.system(" python3 geneidscript.py")
+		softwares_list=request.POST.getlist('softwares[]')        
+		if("1" in softwares_list or len(softwares_list)==0):
+			print("s1")
+			os.path.abspath('..')
+			os.chdir("static/upload/")
+			os.system(" python3 geneidscript.py")
+
+		if("2" in softwares_list):
+			print("s2")
+		if("3" in softwares_list):
+			print("s3")
+		x=request.POST["species"]
 		send_email(request.POST['email'])
 		return HttpResponse("File uploaded successfuly")  
 	else:  
