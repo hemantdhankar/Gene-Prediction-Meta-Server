17f8bc1e392621fd50b59954ea17d0cd3bcef847
Hemant Dhankar <hemantdhankar123@gmail.com>
2019-11-28 22:12:09 +0530

    submitted page linked

--- GPMS_django_project/gene_prediction_meta_server_project/views.py
+++ GPMS_django_project/gene_prediction_meta_server_project/views.py
@@ -8,6 +8,6 @@ def home(request):
 		background_thread=Process(target=run_task, args=(request,))
 		background_thread.daemon=True
 		background_thread.start()
-		return HttpResponse(request,"submitted.html")  
+		return render(request,"submitted.html")  
 	else:
 		return render(request,"home.html")
