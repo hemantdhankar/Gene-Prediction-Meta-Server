5d36b39fde8443b76d9d1b6bbf0abbd27939dfe8
Hemant Dhankar <43892160+TheWizard25@users.noreply.github.com>
2020-01-11 23:32:30 +0530

    Update settings.py

--- GPMS_django_project/gene_prediction_meta_server_project/settings.py
+++ GPMS_django_project/gene_prediction_meta_server_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y+qsb-=oc$3emcb265v&9=hw@cqhz1q2%0*#ei!x&qnkf5-uyc'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
